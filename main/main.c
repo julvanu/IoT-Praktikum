789c090b1e456e032469d96fb4503d56a31e11ae
Julian Hanuschek <julian.hanuschek@tum.de>
2025-10-31 16:38:26 +0100

    Initial commit

---
+++ main/main.c
@@ -0,0 +1,95 @@
+#include "main.h"
+
+#include <stdio.h>
+#include <string.h>
+
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "esp_system.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "gauge.h"
+#include "mqtt.h"
+#include "nvs_flash.h"
+#include "sntp.h"
+#include "wifi.h"
+#include "esp_sleep.h"
+#include "esp_mac.h"
+
+
+void app_main() {
+  ESP_LOGI("progress", "[APP] Free memory: %d bytes", esp_get_free_heap_size());
+  ESP_LOGI("progress", "[APP] IDF version: %s", esp_get_idf_version());
+
+  esp_log_level_set("*", ESP_LOG_ERROR);
+  esp_log_level_set("mqtt", ESP_LOG_INFO);
+  esp_log_level_set("progress", ESP_LOG_INFO);
+  esp_log_level_set("gauge", ESP_LOG_INFO);
+  esp_log_level_set("INFO", ESP_LOG_INFO);
+
+  // getRSOC(); //only if battery!
+  esp_err_t ret = nvs_flash_init();
+  if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    ESP_ERROR_CHECK(nvs_flash_erase());
+    ret = nvs_flash_init();
+  }
+  ESP_ERROR_CHECK(ret);
+
+  ESP_LOGI("INFO", "Attempting to retrieve MAC-address...");
+  unsigned char mac_base[6] = {0};
+  esp_read_mac(mac_base, ESP_MAC_WIFI_STA);
+  unsigned char mac_address[18];
+  snprintf(mac_address, 18, "%02X:%02X:%02X:%02X:%02X:%02X", mac_base[0],mac_base[1],mac_base[2],mac_base[3],mac_base[4],mac_base[5]);
+  ESP_LOGI("INFO", "MAC-address: %s", mac_address);
+  // ESP_LOGI("INFO", "MAC-address: "MACSTR"", MAC2STR(mac_base));
+
+  ESP_LOGI("progress", "Starting Wifi");
+  start_wifi();
+
+  ESP_LOGI("progress", "Starting Clock");
+  start_clock();
+
+  ESP_LOGI("progress", "Starting MQTT");
+  start_mqtt();
+
+  // Identity check: PIR2
+  unsigned char mac_corridor[] = "99:99:99:99:99:99";
+  unsigned char mac_bathroom[] = "94:b9:7e:54:d3:00";
+  // int n = memcmp ( mac_base, mac_pir2, sizeof(mac_base) );
+  if (0 == memcmp ( mac_address, mac_corridor, sizeof(mac_address) )) {
+    ESP_LOGI("INFO", "DEVICE: corridor");
+    
+    // Battery status code -----------------------------
+    ESP_LOGI("progress", "Sending battery status to MQTT");
+    sendBatteryStatusToMQTT();
+    
+    // PIR sensor code ---------------------------------
+    ESP_LOGI("progress", "Sending PIR event to MQTT");
+    sendPIReventToMQTT();
+  } else if (0 == memcmp ( mac_address, mac_bathroom, sizeof(mac_address) )) {
+    ESP_LOGI("INFO", "DEVICE: bathroom");
+    
+    // PIR sensor code ---------------------------------
+    ESP_LOGI("progress", "Sending PIR event to MQTT");
+    sendPIReventToMQTT();
+  } else {
+    ESP_LOGI("INFO", "DEVICE: UNKNOWN");
+  }
+
+
+  // PIR sensor code ---------------------------------
+  ESP_LOGI("progress", "Sending PIR event to MQTT");
+  sendPIReventToMQTT();
+
+  ESP_ERROR_CHECK(gpio_set_direction(PIR_PIN, GPIO_MODE_INPUT));
+  ESP_ERROR_CHECK(rtc_gpio_pulldown_en(PIR_PIN));
+
+  // sleep and wake up
+  ESP_LOGI("progress", "Installing wakeup");
+  while (gpio_get_level(PIR_PIN)==1){
+    vTaskDelay(pdMS_TO_TICKS(1000));
+  }
+  ESP_ERROR_CHECK(esp_sleep_enable_ext0_wakeup(PIR_PIN, 1));
+  ESP_LOGI("progress", "Going to sleep");
+  esp_deep_sleep_start();
+}
---
+++ main/main.h
@@ -0,0 +1,25 @@
+#pragma once
+
+
+// #define EXAMPLE_ESP_WIFI_SSID      "CAPS-Seminar-Room"
+// #define EXAMPLE_ESP_WIFI_PASS      "caps-schulz-seminar-room-wifi"
+// #define SNTP_SERVER_NAME           "ntp1.in.tum.de"
+
+#define EXAMPLE_ESP_WIFI_SSID      "Raum der Wuensche"
+#define EXAMPLE_ESP_WIFI_PASS      "2Affenimboot"
+#define SNTP_SERVER_NAME           "pool.ntp.org"
+
+#define MQTT_BROKER                "192.168.178.41" // your RPi IP address (WLAN: .39, LAN: .41)
+
+// DEVICE: corridor
+// #define DEVICE_ID                  "1"
+// #define DEVICE_TOPIC               "1/1/data"
+// #define DEVICE_KEY                 "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3NjE0OTUxNTUsImlzcyI6ImlvdHBsYXRmb3JtIiwic3ViIjoiMS8xIn0.3Y8iQ6IFmdMR961UvjVm3VfhKQFl70BXhpHC6zgRf4Sy2NFra90_NBH9FaBb-mbiV23napmvwAabtxHi7Unx2rAmGj3FI-o6WnM89qBeq9BzfjThyhu20g7giamEyN9F1lYp3U-s9sk8Uw3EAkKOpFK2tD4_5uK5awgIduStIT_ikgeJDViRg0G-HUISYNoNNXQbjMnS9WfwtTd8xTSDis8FW7HDu3tfcLy3KBc1AvPhy40zOia3Vptgq7x6SkL1sX9bA-qZGBfmQ3h2fGTcncmOF3bXplmtg5VfFhTXRmtywV9j7ydem6EzeYXQMSXFbMBjPAZtbLrbyIDsnsyKrIW1Be_ii7lcFY0RALgHhzJTCRQKONmEo2XXV83zEL4n8RQOX9gBFSgHaksV0qtibLtx7NvbeF0Owu3kbEiAimlJhQyHwstLcx4KxPBjE6DD5Na93QB7Z3Hk-ioWjpXc8BjdFfVOJgr9TOeVnr04fmVtHduvOwgYnzvynDz4fHcmXHATShaXMt1eUHds9bt1S3VVHJPRUpW-Yw_XLLrqrN34aPu4NVPw20aNrpBVDZcZFCHNqaAn1UM6glF2_5XPbcnNM7dZkQ39pLM9D5iz7LhQ6tSWyWw3Vjy5gWOaCQFPpkKUwN_2tU-5i6x_lVttfPiHK4eTkS0SKNaOMY8zg1U"
+
+// DEVICE: bathroom
+#define DEVICE_ID                  "2"
+#define DEVICE_TOPIC               "1/2/data"
+#define DEVICE_KEY                 "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3NjE5MjI0MzgsImlzcyI6ImlvdHBsYXRmb3JtIiwic3ViIjoiMS8yIn0.vdvpgq7d0bop9pU_dEXgyqHI06SsG2IC3g4IyxIHewunqhdC-DRjgM-r82Yt--sRiOFJoHuXsH4VczEML29uxUSxnxwlEkeqHrNZvudJtOscaxuOUCydm9oIoq2AqHpltBHuftnvyIO8Wpt8EJ_DCjwBdkum34LwAnJa_3cZE03UrM5sWyglucGYonWZwcdjqIOGELJafsdPwvVlNIdE0hMlpbTWd9WVDhER8JGUrQHaH9EqbsymM_bhowjMJgdIi4Bf0BlrZjGvGfl3eFudAeZpoEa_X-zWKqGkPESgjIloEC6ex4XHhyFBq1mRL3UzoYfTEON7O9jOsaBATCgFAFBA6oL7ssdh7oaGJE8TciwEUcQKdByIRgQ9dYcbE8-KAavC7rgUMr-poKf7ZqWcuKrfiMEVSIbJGTAzL5GWbWFtYs9HDT47RUOk1mavNHTeK1u2OHu2ELW22DB0u_te5GSunonzrPWvOAagJuPDx5L0GD2nJ-nSAAgwhN8icgzPRuYa2eRro40sLyc3ndOKC35uC9txxf-g2o_MgLkzW1bBfU8NDnFm6QCbZGbYKgcaFQjipzn5U0juz6fiBsA5KBJMhNxUWR8gAhXg2CVuxgu0Gzc8b4wrTr-wC53i6Q2JM6L9TBa6sb92k5f2adsBd6SS5KX559Dy_v1Woa44L6k"
+
+
+#define PIR_PIN 27 
