38ed9c0976c79976a71d91881891ba88ca96153c
Julian Hanuschek <julian.hanuschek@tum.de>
2025-12-21 23:50:43 +0100

    feat: finalize stays analysis and respective docker image

--- scripts/coarsen_data.py
+++ scripts/coarsen_data.py
@@ -1,17 +1,7 @@
-"""
-Complete InfluxDB 2.x ETL Example Script
-----------------------------------------
-This script demonstrates:
-  - Connecting to InfluxDB
-  - Querying data
-  - Converting to pandas
-  - Running analysis
-  - Writing results back to InfluxDB
-"""
-
 from influxdb_client import InfluxDBClient
 import pandas as pd
-from datetime import timedelta
+from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
 
 from influxdb_client.client.write_api import SYNCHRONOUS
 
@@ -24,6 +14,8 @@ INFLUX_ORG = "e35fd59963c483cd"
 INFLUX_BUCKET_CORRIDOR = "1_1_1"
 INFLUX_BUCKET_BATHROOM = "1_2_6"
 INFLUX_BUCKET_KITCHEN = "1_4_10"
+INFLUX_BUCKET_LIVINGROOM = "1_5_11"
+INFLUX_BUCKET_BEDROOM = "1_6_12"
 INFLUX_BUCKET_AUTH = ""
 
 CONFIG_STAY_MAX_INTERVAL = 10
@@ -36,8 +28,11 @@ def get_bucket_from_device(device_id: int):
         bucket = INFLUX_BUCKET_BATHROOM
     elif device_id == 4:
         bucket = INFLUX_BUCKET_KITCHEN
+    elif device_id == 5:
+        bucket = INFLUX_BUCKET_LIVINGROOM
+    elif device_id == 6:
+        bucket = INFLUX_BUCKET_BEDROOM
     else:
-        print("Unknown device ID.")
         return None
     return bucket
 
@@ -56,17 +51,18 @@ def get_client():
 # ============================================================
 # 3. QUERY DATA FROM INFLUXDB
 # ============================================================
-def query_data(device_id=1,  start=None, stop=None, range_in_hours=None):
-    query_api = client.query_api()
-    MEASUREMENT_PIR = "PIR"
+def query_all_device_data(start: str, stop: str):
+    df = pd.DataFrame()
+    for device_id in [1, 2, 4, 5, 6]:
+        df_temp = query_data(device_id=device_id, start=start, stop=stop)
+        df = pd.concat([df, df_temp], ignore_index=True)
+    return df
 
-    if device_id == 1:
-        bucket = INFLUX_BUCKET_CORRIDOR
-    elif device_id == 2:
-        bucket = INFLUX_BUCKET_BATHROOM
-    elif device_id == 4:
-        bucket = INFLUX_BUCKET_KITCHEN
-    else:
+def query_data(device_id=1, start=None, stop=None, range_in_hours=None):
+    query_api = client.query_api()
+
+    bucket = get_bucket_from_device(device_id)
+    if bucket is None:
         print("Unknown device ID.")
         return
 
@@ -74,16 +70,18 @@ def query_data(device_id=1,  start=None, stop=None, range_in_hours=None):
         query = f'''
                from(bucket: "{bucket}")
                  |> range(start: {start}, stop: {stop})
-                 |> filter(fn: (r) => r["_measurement"] == "{MEASUREMENT_PIR}")
+                 |> filter(fn: (r) => r["_measurement"] == "PIR")
                  |> keep(columns: ["_time", "_value", "_field", "_measurement"])
+            |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
            '''
-       ###### |> pivot(rowKey: ["_time"], columnKey: ["_field"], valueColumn: "_value") ############
+    ###### |> pivot(rowKey: ["_time"], columnKey: ["_field"], valueColumn: "_value") ############
     elif range_in_hours is not None:
         query = f'''
             from(bucket: "{bucket}")
               |> range(start: -{range_in_hours}h)
-              |> filter(fn: (r) => r["_measurement"] == "{MEASUREMENT_PIR}")
+              |> filter(fn: (r) => r["_measurement"] == "PIR")
               |> keep(columns: ["_time", "_value", "_field", "_measurement"])
+            |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
         '''
     else:
         print("You need to provide either start and stop time or a range in hours.")
@@ -96,26 +94,68 @@ def query_data(device_id=1,  start=None, stop=None, range_in_hours=None):
     if isinstance(df, list):
         df = pd.concat(df, ignore_index=True)
 
-    print(f"Retrieved {len(df)} rows.")
+    print(f"\nFound {df.shape[0]} rows of data for device {device_id}")
     return df
 
 
 # ============================================================
 # 4. PROCESS DATA
 # ============================================================
-def add_to_df(df, start_time, latest_time, room):
-    duration = pd.Timedelta(seconds=0) if latest_time is None else latest_time - start_time
+def add_to_df(df, start_time: pd.Timestamp, end_time: pd.Timestamp, room):
+    duration = pd.Timedelta(seconds=0) if end_time is None else end_time - start_time
     print(f"Duration: {duration.seconds}")
     df.loc[len(df)] = [room, start_time, duration.seconds]
 
 
-def analyze_stays(df: pd.DataFrame):
+def get_previous_room(query_end: pd.Timestamp):
+    print("Querying previous room...")
+    query_api = client.query_api()
+    bucket = "stays"
+    query_end = query_end.to_pydatetime()  # type: datetime
+    query_start = query_end - timedelta(days=1)
+    start = query_start.strftime('%Y-%m-%dT%H:%M:%SZ')
+    stop = query_end.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    print(f"Start: {start}, stop: {stop}")
+
+    query = f'''
+        from(bucket: "{bucket}")
+          |> range(start: {start}, stop: {stop})
+          |> filter(fn: (r) => r["_measurement"] == "room_stays")
+          |> max(column: "_time")
+          |> keep(columns: ["_time", "room", "_value"])
+    '''
+    df_result = query_api.query_data_frame(query)
+    df_result = df_result.sort_values(by="_time", ascending=False)
+    print(df_result)
+    room = df_result.at[0, "room"]
+    print(room)
+    return room
+
+
+# Returns the row of the next event after the given time query_start
+# Does not check, however, whether that event was triggered by a different device than the previous one
+def get_next_event(query_start: pd.Timestamp):
+    print("Querying previous room...")
+    query_api = client.query_api()
+    bucket = "stays"
+    query_start = query_start.to_pydatetime()  # type: datetime
+    query_end = query_start + timedelta(days=1)
+    start = query_start.strftime('%Y-%m-%dT%H:%M:%SZ')
+    stop = query_end.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    df = query_all_device_data(start, stop)
+    if df.empty:
+        return None
+    row = df.loc[df["_time"].idxmin()]
+    return row
+
+
+def analyze_stays(df: pd.DataFrame, query_start: pd.Timestamp, query_stop: pd.Timestamp) -> pd.DataFrame:
     print("\nAnalyzing stays...")
     if df.empty:
         print("\nEmpty dataframe.")
         return df
-
-    room = df.loc[0, '_value']
 
     df_stays = pd.DataFrame({
         "room": [],
@@ -123,34 +163,34 @@ def analyze_stays(df: pd.DataFrame):
         "duration": []
     })
 
-    df.sort_values(by="_time")  # ascending by default
-    time_array = df["_time"]
-    start_time = df.loc[0, '_time']  # type: pd.Timestamp
-    print(f"Start time: {start_time}")
+    df_sorted = df.sort_values(by="_time")  # ascending by default
 
-    print(f"Type of start_time_str: {type(start_time)}")
-    latest_time = None
-    for idx, event_time in enumerate(time_array[1:]):
-        time_passed = event_time - start_time
-        # Check if event is part of the same stay
-        if time_passed < timedelta(minutes=CONFIG_STAY_MAX_INTERVAL):
-            # If part of stay, extend latest_time to current event_time and continue with next event
-            latest_time = event_time
-            continue
-        else:
-            # If not, write stay to df and set current event_time as new start_time
-            add_to_df(df_stays, start_time, latest_time, room)
-            # Reset start_time & latest_time
-            latest_time = None
-            start_time = event_time
+    start_time = df_sorted.iloc[0]["_time"]
+    room = df_sorted.iloc[0]["roomID"]
+    # start_time = query_start  # type: pd.Timestamp
+    # room = get_previous_room(query_start)
 
-    if start_time is not None:
-        add_to_df(df_stays, start_time, latest_time, room)
+    for idx, row in df_sorted.iterrows():
+        if row['roomID'] != room:
+            print(f"Room changed from {room} to {row['roomID']} at time {row['_time']}")
+            # Write previous stay to df
+            add_to_df(df_stays, start_time, row['_time'], room)
+            # Reset start_time & room
+            start_time = row['_time']
+            room = row['roomID']
+
+    # Add stay for last room until end of query window
+    row = get_next_event(query_stop)
+    if row is None:
+        add_to_df(df_stays, start_time, query_stop, room)
+    else:
+        add_to_df(df_stays, start_time, row['_time'], row['roomID'])
+
     return df_stays
 
 
 # ============================================================
-# 5. WRITE RESULTS BACK TO INFLUXDB
+# 5. WRITE STAYS AND ACTIVITIES: BACK TO INFLUXDB
 # ============================================================
 def write_results(df_stays: pd.DataFrame):
     print("\nWriting results to InfluxDB...")
@@ -183,14 +223,19 @@ if __name__ == "__main__":
     print("Connecting to InfluxDB...")
     client = get_client()
 
-    device_id = 1
-    # range_in_hours = 6
-    start = "2025-12-01T00:00:00Z"
-    stop = "2025-12-08T23:59:59Z"
+    # start_date = "2025-12-01"
+    # stop_date = "2025-12-19"
+    stop_date = datetime.now(ZoneInfo("Europe/Berlin")).date() - timedelta(days=1)
+    start_date = stop_date - timedelta(days=1)
 
-    df = query_data(device_id=device_id, start=start, stop=stop)
-    print(f"\nFound {df.shape[0]} rows of data")
-    df_stays = analyze_stays(df)
+    start = f"{start_date}T00:00:00Z"
+    stop = f"{stop_date}T00:00:00Z"
+    print(f"Querying data for period from {start} until {stop}...")
+
+    df = query_all_device_data(start, stop)
+
+    df_stays = analyze_stays(df, query_start=pd.Timestamp(start), query_stop=pd.Timestamp(stop))
     write_results(df_stays)
+    print(df_stays)
 
     print("\nPipeline finished.")
